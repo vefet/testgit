a7b7b4d9ef8e2530afec0f2273ce18e010481076
shilei <2444687728@qq.com>
2023-06-02 21:04:44 +0800

    init

---
+++ src/com/test/Test.java
@@ -0,0 +1,15 @@
+package com.test;
+
+/**
+ * @author 石磊
+ * @ClassName Test
+ * @description: *
+ * @datetime 2023年 06月 02日 21:03
+ * @version: 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
